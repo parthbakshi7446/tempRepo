85f68eb69e5f697ed1556d0967f559f39127578a
parthbakshi7446 <parthbakshi7446@gmail.com>
2022-09-20 23:42:35 +0530

    save

---
+++ index2.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName("button-click")[0].addEventListener('click', ()=> {
+    const collection = document.getElementsByClassName("small-box");
+    for (let i = 0; i < collection.length; i++) {
+        if(collection[i].style.backgroundColor=="blue") {
+            collection[i].style.backgroundColor = "green";
+        } else{
+            collection[i].style.backgroundColor = "blue";
+        }
+    
+    }
+});
+console.log(document.getElementsByClassName("small-box"));
